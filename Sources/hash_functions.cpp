5bd554d19c0d911fee1951c4f9ff655406d1634a
marto55 <martin_my@abv.bg>
2023-12-27 18:17:35 +0200

    add hesh function for class string

--- Sources/hash_functions.cpp
+++ Sources/hash_functions.cpp
@@ -1,11 +1,14 @@
 #include "hash_functions.h"
+#include <string>
+
+using std::string;
 
 #define A 54059 /* a prime */
 #define B 76963 /* another prime */
 #define C 86969 /* yet another prime */
 #define FIRSTH 37 /* also prime */
 
-unsigned hash_string(const char* s)
+unsigned hash_c_string(const char* s)
 {
    unsigned h = FIRSTH;
    while (*s) {
@@ -13,4 +16,25 @@ unsigned hash_string(const char* s)
      s++;
    }
    return h; // or return h % C;
+}
+
+int pow (int x, unsigned int y)
+{
+    if (y == 0)
+        return 1;
+    else if ((y % 2) == 0)
+        return pow (x, y / 2) * pow (x, y / 2);
+    else
+        return x * pow (x, y / 2) * pow (x, y / 2);
+
+}
+
+const int PRIME_CONST = 31;
+
+unsigned hash_string(string key) {
+    int hashCode = 0;
+    for (unsigned i = 0; i < key.length(); i++) {
+        hashCode += key[i] * pow(PRIME_CONST, i);
+    }
+    return hashCode;
 }
--- include/hash_functions.h
+++ include/hash_functions.h
@@ -1,6 +1,10 @@
 #ifndef HSH_H
 #define HSH_H
 
-unsigned hash_string(const char* s);
+#include <string>
+using std::string;
+
+unsigned hash_c_string(const char* s);
+unsigned hash_string(string s);
 
 #endif // HSH_H
