3e91798e2c7e89e05f9d2d5e517eb6229b4b122a
marto55 <martin_my@abv.bg>
2023-12-31 15:52:40 +0200

    create_table() can now read the whole query

--- include/table.h
+++ include/table.h
@@ -1,12 +1,17 @@
 #ifndef TBL_H
 #define TBL_H
 
+#include <string>
 #include "column.h"
 
+using std::string;
+
 class Table{
-public:
-	char name[STRING_SIZE];
+private:
+    char name[DATABASE_STRING_SIZE];
     char columns[ MAX_NUMBER_OF_COLUMNS * COLUMN_SIZE_IN_BYTES];
+public:
+	void init(string name, Column columns[MAX_NUMBER_OF_COLUMNS]);
 };
 
 #endif // TBL_H
